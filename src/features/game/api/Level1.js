22401be039991c1adfb22546b506078798b9c15b
MohitGoyal-MG <54404911+MohitGoyal-MG@users.noreply.github.com>
2023-07-21 23:38:57 +0530

    Update Level1.js

--- src/features/game/api/Level1.js
+++ src/features/game/api/Level1.js
@@ -140,7 +140,6 @@ export default function Typingjutsu() {
       </p>
       <input
         className="w-full border-black border px-4 py-2"
-        onInput={handlePause}
         onChange={(text) => setText(text.target.value)}
         value={text}
         disabled={gameState == GameState.VIEW_STATS}
